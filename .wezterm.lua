0994d7319700048da3474199dced154650413233
Tobias Klug <tbs.klg@gmail.com>
2024-11-16 20:28:07 +0100

    Update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,14 +1,80 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
-config.enable_tab_bar = false
-config.color_scheme = 'Gruvbox dark, hard (base16)'
-
-config.font = wezterm.font_with_fallback {
-  'JetBrains Mono',
-  'Nerd Fonts Symbols',
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		key = "|",
+		mods = "LEADER",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "9",
+		mods = "LEADER",
+		action = act.ShowLauncherArgs({
+			flags = "FUZZY|WORKSPACES",
+		}),
+	},
+	{
+		key = "t",
+		mods = "SHIFT|ALT",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "m",
+		mods = "LEADER",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	{ key = "h", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "j", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "k", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "l", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	{ key = "i", mods = "CTRL|SHIFT", action = act.SwitchToWorkspace },
+	{ key = "n", mods = "CTRL", action = act.SwitchWorkspaceRelative(1) },
+	{ key = "p", mods = "CTRL", action = act.SwitchWorkspaceRelative(-1) },
+	{
+		key = "h",
+		mods = "LEADER",
+		action = act.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = act.AdjustPaneSize({ "Down", 5 }),
+	},
+	{ key = "k", mods = "LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{
+		key = "l",
+		mods = "LEADER",
+		action = act.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
 }
+
+config.enable_tab_bar = true
+config.color_scheme = "Gruvbox dark, hard (base16)"
+
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono",
+	"Nerd Fonts Symbols",
+})
 config.font_size = 15
 
 return config
