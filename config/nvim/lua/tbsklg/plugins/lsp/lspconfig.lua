05ad53347809ac9a473ae41b0f92a3683162ad4c
Tobias Klug <tbs.klg@gmail.com>
2025-07-11 06:30:00 +0200

    Config diagnostic to use new api

--- config/nvim/lua/tbsklg/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/tbsklg/plugins/lsp/lspconfig.lua
@@ -88,12 +88,19 @@ return {
 
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
-        local signs =
-            { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = " ",
+                    [vim.diagnostic.severity.WARN] = " ",
+                    [vim.diagnostic.severity.INFO] = " ",
+                    [vim.diagnostic.severity.HINT] = "󰠠 ",
+                },
+                linehl = {
+                    [vim.diagnostic.severity.ERROR] = "Error",
+                },
+            },
+        })
 
         vim.diagnostic.config({
             virtual_text = true,
